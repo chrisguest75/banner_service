46d3ab3e4e5091205ace1bd3648723febfa4ba7d
Chris Guest <chris.guest@condenastint.com>
2021-02-22 01:23:08 +0000

    Add custom metrics

---
+++ app/ready.py
@@ -0,0 +1,8 @@
+from connexion import NoContent
+from app import metrics
+
+@metrics.summary('reasy_by_status', 'ready Request latencies by status', labels={
+    'code': lambda r: r.status_code
+})
+def ready():
+    return "Ready", 200
